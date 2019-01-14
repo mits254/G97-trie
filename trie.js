14d7c1f16bc26a2927478b8930f410f615eca0dc
Mitali Mittal <mits254@gmail.com>
2019-01-13 22:30:08 -0600

    add insert method

--- trie.js
+++ trie.js
@@ -1,15 +1,28 @@
 class Node {
-  constructor() {
+  constructor(key) {
+    this.key = key;
+    this.end = false;
+    this.children = {}
   }
+  
 }
 
 class Trie {
   constructor() {
-
+   this.root = new Node(null);
   }
 
-  insert(word) {
-    
+  insert(word, node = this.root) {
+    if(word.length == 0){
+      node.end = true;
+      return;
+    } else if(!node.children[word[0]]){
+      node.children[word[0]] = new Node(word[0]);
+      return this.insert(word.substr(1),node.children[word[0]]);
+    } else {
+      return this.insert(word.substr(1),node.children[word[0]]);
+    }
+   
   }
 
   contains(word) {
